a9f1aff108d02bb79c13a4235122417faadd5f11
Glauton Osório <glautonleao@gmail.com>
2023-10-15 17:03:02 -0300

    Erro de importação

--- LifeCare/src/routes/AppRoutes.jsx
+++ LifeCare/src/routes/AppRoutes.jsx
@@ -1,22 +1,23 @@
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { PatientRegistrationPage } from "../pages/PatientRegistrationPage/PatientRegistration.page";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { LoginPage } from '../pages/LoginPage/Login.page.jsx';
-import { Layout } from '../layouts/Layout';
+import { LoginPage } from "../pages/LoginPage/Login.page.jsx";
+import { Layout } from "../layouts/Layout";
 
 export const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-        <Route path='login' element={<LoginPage/>}/>
-          <Route path="/cadastro/paciente" element={<PatientRegistrationPage />} />            
-          <Route path="/cadastro/paciente/:id" element={<PatientRegistrationPage />} />
-        <Route path='/' element={<Layout/>}>
-
-        </Route>
+        <Route path="login" element={<LoginPage />} />
+        <Route
+          path="/cadastro/paciente"
+          element={<PatientRegistrationPage />}
+        />
+        <Route
+          path="/cadastro/paciente/:id"
+          element={<PatientRegistrationPage />}
+        />
+        <Route path="/" element={<Layout />}></Route>
       </Routes>
     </Router>
   );
-}
-
+};
