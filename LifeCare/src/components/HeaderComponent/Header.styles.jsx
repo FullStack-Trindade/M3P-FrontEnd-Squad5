20ef3bbae1d3cd948443ef50f735dddb6fce5892
Laercio Luiz Barbosa <llbarbosa72@hotmail.com>
2023-10-15 22:41:30 -0300

    [FRONT-004] Criação de Componente de Header.

---
+++ LifeCare/src/components/HeaderComponent/Header.styles.jsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  height: 75px;
+  background-color: #8ae9f0;
+  margin-bottom: 10px;
+  display: flex;
+`;
+
+export const Nav = styled.nav`
+  display: flex;
+  width: 100%;
+`;
+
+export const List = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export const ItemList = styled.li`
+  display: flex;
+  text-decoration: none;
+  padding: 20px;
+  font-weight: bold;
+`;
+
+export const Icon = styled.li`
+  display: flex;
+  text-decoration: none;
+  font-size: 40px;
+  align-items: center;
+
+    
+
+`
+
+//export const AppLogo = styled.img
--- LifeCare/src/main.jsx
+++ LifeCare/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { GlobalStyle } from "./global.style.jsx";
+import { GlobalStyle } from "./Global.style.jsx";
 import { AppRoutes } from "./routes/AppRoutes.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
