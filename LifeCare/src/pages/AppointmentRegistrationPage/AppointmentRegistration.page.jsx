43445bf4abfccaba9641c2699fa793eb821969d5
Glauton Osório <glautonleao@gmail.com>
2023-10-23 21:30:09 -0300

    feat: Pagina de consulta feita

---
+++ LifeCare/src/pages/AppointmentRegistrationPage/AppointmentRegistration.page.jsx
@@ -0,0 +1,9 @@
+import { FormRegisterAppointment } from "../../components/Form/FormRegisterAppointment/FormRegisterAppointment.component";
+
+export const AppointmentRegistrationPage = () => {
+  return (
+    <div>
+      <FormRegisterAppointment />
+    </div>
+  );
+};
