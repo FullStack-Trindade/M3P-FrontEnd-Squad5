bad65ef961490e9a6a691b94483875b404d379c4
Glauton Osório <glautonleao@gmail.com>
2023-10-28 05:23:48 -0300

    feat: Todas as rotas de consulta feita

--- LifeCare/src/services/Appointment/Appointment.service.jsx
+++ LifeCare/src/services/Appointment/Appointment.service.jsx
@@ -2,6 +2,11 @@ import axios from "axios";
 
 let API_URL = "http://localhost:3333/api/consultas";
 
+const GetID = async (id) => {
+  const appointmentData = await axios.get(`${API_URL}/${id}`);
+  return appointmentData.data.data;
+};
+
 const Store = async (newData) => {
   axios
     .post(API_URL, {
@@ -21,7 +26,40 @@ const Store = async (newData) => {
       alert(`Erro ao cadastrar ${err.message}`);
     });
 };
+const Update = async (id, newData) => {
+  axios
+    .put(`${API_URL}/${id}`, {
+      appointmentReason: newData.appointmentReason,
+      appointmentDate: newData.appointmentDate,
+      appointmentTime: newData.appointmentTime,
+      description: newData.description,
+      prescriptionMedication: newData.prescriptionMedication,
+      dosagePrecautions: newData.dosagePrecautions,
+      patientId: newData.patientId,
+      userId: newData.userId,
+    })
+    .then((res) => {
+      alert("Atualizado com sucesso");
+    })
+    .catch((err) => {
+      alert(`Erro ao atualizar ${err.message}`);
+    });
+};
+
+const Delete = async (id) => {
+  await axios
+    .delete(`${API_URL}/${id}`)
+    .then((res) => {
+      alert("Deletado com sucesso");
+    })
+    .catch((err) => {
+      alert(`Erro ao deletar ${err.message}`);
+    });
+};
 
 export const Appointment = {
   Store,
+  GetID,
+  Update,
+  Delete,
 };
