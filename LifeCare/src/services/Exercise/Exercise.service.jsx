13d8d7659982dd3d0bf588f5e1c788414167fe6b
Glauton Osório <glautonleao@gmail.com>
2023-10-31 03:54:33 -0300

    feat: Exercise service criado

---
+++ LifeCare/src/services/Exercise/Exercise.service.jsx
@@ -0,0 +1,71 @@
+import axios from "axios";
+
+let API_URL = "http://localhost:3333/api/exercicios";
+
+const Get = async (id) => {
+  const exerciseData = await axios.get(API_URL);
+  return exerciseData.data.data;
+};
+
+const GetID = async (id) => {
+  const exerciseData = await axios.get(`${API_URL}/${id}`);
+  return exerciseData.data.data;
+};
+
+const Store = async (newData) => {
+  axios
+    .post(API_URL, {
+      exerciseName: newData.exerciseName,
+      date: newData.date,
+      time: newData.time,
+      exerciseType: newData.exerciseType,
+      quantityPerWeek: newData.quantityPerWeek,
+      description: newData.description,
+      patientId: newData.patientId,
+      userId: newData.userId,
+    })
+    .then((res) => {
+      alert(res);
+    })
+    .catch((err) => {
+      alert(`Erro ao cadastrar ${err.message}`);
+    });
+};
+const Update = async (id, newData) => {
+  axios
+    .put(`${API_URL}/${id}`, {
+      exerciseName: newData.exerciseName,
+      date: newData.date,
+      time: newData.time,
+      exerciseType: newData.exerciseType,
+      quantityPerWeek: newData.quantityPerWeek,
+      description: newData.description,
+      patientId: newData.patientId,
+      userId: newData.userId,
+    })
+    .then((res) => {
+      alert(res);
+    })
+    .catch((err) => {
+      alert(`Erro ao atualizar ${err.message}`);
+    });
+};
+
+const Delete = async (id) => {
+  await axios
+    .delete(`${API_URL}/${id}`)
+    .then((res) => {
+      alert(res);
+    })
+    .catch((err) => {
+      alert(`Erro ao deletar ${err.message}`);
+    });
+};
+
+export const Exercise = {
+  Store,
+  GetID,
+  Get,
+  Update,
+  Delete,
+};
