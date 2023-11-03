9f528ccf7e6facabe4da99ba35fa843eb4f16480
Glauton Osório <glautonleao@gmail.com>
2023-11-02 21:03:32 -0300

    fix: Atualizando o service de exames

--- LifeCare/src/services/Exam/Exam.service.jsx
+++ LifeCare/src/services/Exam/Exam.service.jsx
@@ -1,73 +1,96 @@
-import axios from "axios";
+import { axiosInstance } from "../../helper/axiosInstance";
 
-let API_URL = "http://localhost:3333/api/exames";
+export const GetExams = async () => {
+  try {
+    const examData = await axiosInstance
+      .get(`/exames`, {
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      })
+      .then((r) => {
+        return r.data;
+      })
+      .catch((e) => console.log(e));
 
-const Get = async () => {
-  const examData = await axios.get(API_URL);
-  return examData.data.data;
+    return examData;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const GetID = async (id) => {
-  const examData = await axios.get(`${API_URL}/${id}`);
-  return examData.data.data;
+export const GetExamID = async (id) => {
+  try {
+    const examData = await axiosInstance
+      .get(`/exames/${id}`, {
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      })
+      .then((r) => {
+        return r.data;
+      })
+      .catch((e) => console.log(e));
+
+    return examData;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const Store = async (newData) => {
-  axios
-    .post(API_URL, {
-      name: newData.name,
-      examDate: newData.examDate,
-      examTime: newData.examTime,
-      examType: newData.examType,
-      laboratory: newData.laboratory,
-      documentURL: newData.documentURL,
-      results: newData.results,
-      patientId: newData.patientId,
-      userId: newData.userId,
+export const StoreExam = async (newData) => {
+  const data = {
+    name: newData.name,
+    examDate: newData.examDate,
+    examTime: newData.examTime,
+    examType: newData.examType,
+    laboratory: newData.laboratory,
+    documentURL: newData.documentURL,
+    results: newData.results,
+    patientId: newData.patientId,
+    userId: newData.userId,
+  };
+  await axiosInstance
+    .post(`/exames`, data, {
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
     })
     .then((res) => {
       alert("Cadastrado com sucesso");
     })
     .catch((err) => {
+      console.log("err: ", err);
       alert(`Erro ao cadastrar ${err.message}`);
     });
 };
-const Update = async (id, newData) => {
-  axios
-    .put(`${API_URL}/${id}`, {
-      name: newData.name,
-      examDate: newData.examDate,
-      examTime: newData.examTime,
-      examType: newData.examType,
-      laboratory: newData.laboratory,
-      documentURL: newData.documentURL,
-      results: newData.results,
-      patientId: newData.patientId,
-      userId: newData.userId,
+export const UpdateExam = async (id, newData) => {
+  const data = {
+    name: newData.name,
+    examDate: newData.examDate,
+    examTime: newData.examTime,
+    examType: newData.examType,
+    laboratory: newData.laboratory,
+    documentURL: newData.documentURL,
+    results: newData.results,
+    patientId: newData.patientId,
+    userId: newData.userId,
+  };
+  await axiosInstance
+    .put(`/exames/${id}`, data, {
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
     })
     .then((res) => {
       alert("Atualizado com sucesso");
     })
     .catch((err) => {
+      console.log("err: ", err);
       alert(`Erro ao atualizar ${err.message}`);
     });
 };
-
-const Delete = async (id) => {
-  await axios
-    .delete(`${API_URL}/${id}`)
+export const DeleteExam = async (id) => {
+  await axiosInstance
+    .delete(`/exames/${id}`, {
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+    })
     .then((res) => {
       alert("Deletado com sucesso");
     })
     .catch((err) => {
       alert(`Erro ao deletar ${err.message}`);
     });
-};
-
-export const Exam = {
-  Store,
-  Get,
-  GetID,
-  Update,
-  Delete,
 };
