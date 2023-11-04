eda9750cf4750b7a6ca22cff4745ec8b423717be
Glauton Osório <glautonleao@gmail.com>
2023-11-04 20:57:54 -0300

    feat: Criando o contexto de tema

--- LifeCare/src/main.jsx
+++ LifeCare/src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import { GlobalStyle } from "./Global.style.jsx";
 import { AppRoutes } from "./routes/AppRoutes.jsx";
 import { HeaderProvider } from "./contexts/HeaderContext/Header.context.jsx";
+import { ThemeProvider } from "./contexts/ThemeContext/Theme.context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HeaderProvider>
-      <GlobalStyle />
-      <AppRoutes />
-    </HeaderProvider>
+    <ThemeProvider>
+      <HeaderProvider>
+        <GlobalStyle />
+        <AppRoutes />
+      </HeaderProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
