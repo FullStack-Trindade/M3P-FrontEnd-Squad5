c93d02658b723a1f2086543ba012b3ca5ac11ad2
CesarNahra <cesarsantanna.nahra@gmail.com>
2023-11-05 18:59:13 -0300

    implementação loader entre páginas

--- LifeCare/src/layouts/Layout.jsx
+++ LifeCare/src/layouts/Layout.jsx
@@ -2,11 +2,20 @@ import { Outlet } from "react-router-dom";
 import { HeaderComponent } from "../components/HeaderComponent/Header.component";
 import * as Styled from "./Layout.styles";
 import { SidebarComponent } from "../components/SidebarComponent/Sidebar.component";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { HeaderContext } from "../contexts/HeaderContext/Header.context";
 
 export const Layout = () => {
   const { title } = useContext(HeaderContext);
+  const [showContent, setShowContent] = useState(false)
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowContent(true)
+    }, 3000)
+  })
+
   return (
     <Styled.App>
       <Styled.Main>
@@ -14,7 +23,11 @@ export const Layout = () => {
         <SidebarComponent
           children={
             <Styled.Content>
-              <Outlet />
+              {showContent ? (
+                <Outlet />
+              ) : (
+                <Styled.Paragraph>Loading...</Styled.Paragraph>
+              )}
             </Styled.Content>
           }
         />
