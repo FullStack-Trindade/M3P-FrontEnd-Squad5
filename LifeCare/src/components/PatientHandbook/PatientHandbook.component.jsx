3bdd4ebe97fc52a3624c6a72076b2ed78a770907
Glauton Osório <glautonleao@gmail.com>
2023-11-05 22:58:41 -0300

    fix: Style de prontuario paciente

--- LifeCare/src/components/PatientHandbook/PatientHandbook.component.jsx
+++ LifeCare/src/components/PatientHandbook/PatientHandbook.component.jsx
@@ -1,6 +1,8 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { GetRecordsID } from "../../services/MedicalRecord/MedicalRecord.service";
+import * as Styled from "./PatientHandbook.styles";
+import { ThemeContext } from "../../contexts/ThemeContext/Theme.context";
 
 export const PatientHandbookComponent = () => {
   const { id } = useParams();
@@ -10,6 +12,7 @@ export const PatientHandbookComponent = () => {
   const [diet, setDiet] = useState();
   const [medicine, setMedicine] = useState();
   const [exams, setExam] = useState();
+  const { theme, setTheme } = useContext(ThemeContext);
 
   useEffect(() => {
     if (id) {
@@ -28,160 +31,238 @@ export const PatientHandbookComponent = () => {
   }, []);
 
   return (
-    <section>
+    <Styled.Form $colors={theme.cores}>
       {patient && (
-        <div className="patientRecordsContent" key={patient.id}>
-          <h1> {patient.fullName} </h1>
-          <span>Convênio: {patient.healthInsurance} </span>
-          <span>Telefone: {patient.emergency} </span>
-          <span>Alergia: {patient.listOfAllergies} </span>
-          <span>Cuidado Especifico: {patient.specificCare} </span>
-        </div>
+        <Styled.FormColumn key={patient.id}>
+          <Styled.FormTitle $colors={theme.texto}>
+            {" "}
+            {patient.fullName}{" "}
+          </Styled.FormTitle>
+          <Styled.FormSubTitle $colors={theme.texto}>
+            Convênio: {patient.healthInsurance}{" "}
+          </Styled.FormSubTitle>
+          <Styled.FormSubTitle $colors={theme.texto}>
+            Telefone: {patient.emergencyContact}{" "}
+          </Styled.FormSubTitle>
+          <Styled.FormSubTitle $colors={theme.texto}>
+            Alergia: {patient.listOfAllergies}{" "}
+          </Styled.FormSubTitle>
+          <Styled.FormSubTitle $colors={theme.texto}>
+            Cuidado Especifico: {patient.specificCare}{" "}
+          </Styled.FormSubTitle>
+        </Styled.FormColumn>
       )}
 
-      <div className="patientRecordsContent">
-        <h1>Consultas</h1>
+      <Styled.FormColumn>
+        <Styled.FormTitle $colors={theme.texto}>Consultas</Styled.FormTitle>
 
         <div>
           {appointments &&
             appointments.map((appointment) => {
               return (
-                <div className="patientRecordsContent" key={appointment.id}>
-                  <div className=" PatientRecordsRow">
-                    <h2>Motivo da Consulta: {appointment.appointmentReason}</h2>
+                <div key={appointment.id}>
+                  <Styled.FormRow>
+                    <Styled.FormTitle $colors={theme.texto}>
+                      Motivo da Consulta: {appointment.appointmentReason}
+                    </Styled.FormTitle>
                     <Link
                       className="LinkPatientRecord"
                       to={`/cadastro/consulta/${appointment.id}`}
                     >
                       Editar
                     </Link>
-                  </div>
-                  <span>Descrição:</span>
-                  <p> {appointment.description}</p>
-                  <span>Receita prescrevida:</span>
-                  <p> {appointment.prescriptionMedication} </p>
-                  <p> {appointment.dosagePrecautions} </p>
-                  <div>
-                    <span>Data da Consulta:{appointment.appointmentDate}</span>
-                    <span>
+                  </Styled.FormRow>
+                  <Styled.FormSubTitle $colors={theme.texto}>
+                    Descrição:
+                  </Styled.FormSubTitle>
+                  <Styled.FormSubTitle $colors={theme.texto}>
+                    {" "}
+                    {appointment.description}
+                  </Styled.FormSubTitle>
+                  <Styled.FormSubTitle $colors={theme.texto}>
+                    Receita prescrevida:
+                  </Styled.FormSubTitle>
+                  <Styled.FormSubTitle $colors={theme.texto}>
+                    {" "}
+                    {appointment.prescriptionMedication}{" "}
+                  </Styled.FormSubTitle>
+                  <Styled.FormSubTitle $colors={theme.texto}>
+                    {" "}
+                    {appointment.dosagePrecautions}{" "}
+                  </Styled.FormSubTitle>
+                  <Styled.ButtonWrapper>
+                    <Styled.FormSubTitle $colors={theme.texto}>
+                      Data da Consulta:{appointment.appointmentDate}
+                    </Styled.FormSubTitle>
+                    <Styled.FormSubTitle $colors={theme.texto}>
                       Horário da Consulta:{appointment.appointmentTime}
-                    </span>
-                  </div>
+                    </Styled.FormSubTitle>
+                  </Styled.ButtonWrapper>
                 </div>
               );
             })}
         </div>
-        <div className="patientRecordsContent">
-          <h1>Exames</h1>
-          {exams &&
-            exams.map((exam) => {
-              return (
-                <div className="patientRecordsContent" key={exam.id}>
-                  <div className=" PatientRecordsRow">
-                    <h2>{exam.laboratory}</h2>
-                    <Link
-                      className="LinkPatientRecord"
-                      to={`/cadastro/exame/${exam.id}`}
-                    >
-                      Editar
-                    </Link>
-                  </div>
-                  <span>{exam.name}</span>
-                  <span>Tipo do Exame:{exam.examType}</span>
-                  <span>Resultado:</span>
-                  <p>{exam.results}</p>
-                  <div>
-                    <span>Data do Exame:{exam.examDate}</span>
-                    <span>Horário do Exame:{exam.examTime}</span>
-                  </div>
-                </div>
-              );
-            })}
-        </div>
-        <div className="patientRecordsContent">
-          <h1>Dietas</h1>
-          {diet &&
-            diet.map((diet) => {
-              return (
-                <div className="patientRecordsContent" key={diet.id}>
-                  <div className=" PatientRecordsRow">
-                    <h2>Nome da Dieta: {diet.name}</h2>
-                    <Link
-                      className="LinkPatientRecord"
-                      to={`/cadastro/dieta/${diet.id}`}
-                    >
-                      Editar
-                    </Link>
-                  </div>
-                  <span>Tipo de Dieta:{diet.dietType}</span>
-                  <span>Descrição da Dieta:{diet.description}</span>
+      </Styled.FormColumn>
 
-                  <div>
-                    <span>Data do Exame:{diet.date}</span>
-                    <span>Horário do Exame:{diet.time}</span>
-                  </div>
-                </div>
-              );
-            })}
-        </div>
-        <div className="patientRecordsContent">
-          <h1>Exercício</h1>
-          {exercise &&
-            exercise.map((exercise) => {
-              return (
-                <div className="patientRecordsContent" key={exercise.id}>
-                  <div className=" PatientRecordsRow">
-                    <h2>
-                      Nome do Série de Exercícios: {exercise.exerciseName}
-                    </h2>
-                    <Link
-                      className="LinkPatientRecord"
-                      to={`/cadastro/exercicio/${exercise.id}`}
-                    >
-                      Editar
-                    </Link>
-                  </div>
-                  <span>Tipo de exercício:{exercise.exerciseType}</span>
-                  <span>Quantidade por Semana:{exercise.quantityPerWeek}</span>
-                  <span>Descrição da Dieta:{exercise.description}</span>
+      <Styled.FormColumn>
+        <Styled.FormTitle $colors={theme.texto}>Exames</Styled.FormTitle>
+        {exams &&
+          exams.map((exam) => {
+            return (
+              <div className="patientRecordsContent" key={exam.id}>
+                <Styled.FormRow>
+                  <Styled.FormTitle $colors={theme.texto}>
+                    {exam.laboratory}
+                  </Styled.FormTitle>
+                  <Link
+                    className="LinkPatientRecord"
+                    to={`/cadastro/exame/${exam.id}`}
+                  >
+                    Editar
+                  </Link>
+                </Styled.FormRow>
+                <Styled.FormSubTitle $colors={theme.texto}>
+                  {exam.name}
+                </Styled.FormSubTitle>
+                <Styled.FormSubTitle $colors={theme.texto}>
+                  Tipo do Exame:{exam.examType}
+                </Styled.FormSubTitle>
+                <Styled.FormSubTitle $colors={theme.texto}>
+                  Resultado:
+                </Styled.FormSubTitle>
+                <Styled.FormSubTitle $colors={theme.texto}>
+                  {exam.results}
+                </Styled.FormSubTitle>
+                <Styled.ButtonWrapper>
+                  <Styled.FormSubTitle $colors={theme.texto}>
+                    Data do Exame:{exam.examDate}
+                  </Styled.FormSubTitle>
+                  <Styled.FormSubTitle $colors={theme.texto}>
+                    Horário do Exame:{exam.examTime}
+                  </Styled.FormSubTitle>
+                </Styled.ButtonWrapper>
+              </div>
+            );
+          })}
+      </Styled.FormColumn>
+      <Styled.FormColumn>
+        <Styled.FormTitle $colors={theme.texto}>Dietas</Styled.FormTitle>
+        {diet &&
+          diet.map((diet) => {
+            return (
+              <div className="patientRecordsContent" key={diet.id}>
+                <Styled.FormRow>
+                  <Styled.FormTitle $colors={theme.texto}>
+                    Nome da Dieta: {diet.name}
+                  </Styled.FormTitle>
+                  <Link
+                    className="LinkPatientRecord"
+                    to={`/cadastro/dieta/${diet.id}`}
+                  >
+                    Editar
+                  </Link>
+                </Styled.FormRow>
+                <Styled.FormSubTitle $colors={theme.texto}>
+                  Tipo de Dieta:{diet.dietType}
+                </Styled.FormSubTitle>
+                <Styled.FormSubTitle $colors={theme.texto}>
+                  Descrição da Dieta:{diet.description}
+                </Styled.FormSubTitle>
 
-                  <div>
-                    <span>Data do Exercício:{exercise.date}</span>
-                    <span>Horário do Exercício:{exercise.time}</span>
-                  </div>
-                </div>
-              );
-            })}
-        </div>
-      </div>
-      <div className="patientRecordsContent">
-        <h1>Medicamento</h1>
+                <Styled.ButtonWrapper>
+                  <Styled.FormSubTitle $colors={theme.texto}>
+                    Data do Exame:{diet.date}
+                  </Styled.FormSubTitle>
+                  <Styled.FormSubTitle $colors={theme.texto}>
+                    Horário do Exame:{diet.time}
+                  </Styled.FormSubTitle>
+                </Styled.ButtonWrapper>
+              </div>
+            );
+          })}
+      </Styled.FormColumn>
+      <Styled.FormColumn>
+        <Styled.FormTitle $colors={theme.texto}>Exercício</Styled.FormTitle>
+        {exercise &&
+          exercise.map((exercise) => {
+            return (
+              <div className="patientRecordsContent" key={exercise.id}>
+                <Styled.FormRow>
+                  <Styled.FormTitle $colors={theme.texto}>
+                    Nome do Série de Exercícios: {exercise.exerciseName}
+                  </Styled.FormTitle>
+                  <Link
+                    className="LinkPatientRecord"
+                    to={`/cadastro/exercicio/${exercise.id}`}
+                  >
+                    Editar
+                  </Link>
+                </Styled.FormRow>
+                <Styled.FormSubTitle $colors={theme.texto}>
+                  Tipo de exercício:{exercise.exerciseType}
+                </Styled.FormSubTitle>
+                <Styled.FormSubTitle $colors={theme.texto}>
+                  Quantidade por Semana:{exercise.quantityPerWeek}
+                </Styled.FormSubTitle>
+                <Styled.FormSubTitle $colors={theme.texto}>
+                  Descrição da Dieta:{exercise.description}
+                </Styled.FormSubTitle>
+
+                <Styled.ButtonWrapper>
+                  <Styled.FormSubTitle $colors={theme.texto}>
+                    Data do Exercício:{exercise.date}
+                  </Styled.FormSubTitle>
+                  <Styled.FormSubTitle $colors={theme.texto}>
+                    Horário do Exercício:{exercise.time}
+                  </Styled.FormSubTitle>
+                </Styled.ButtonWrapper>
+              </div>
+            );
+          })}
+      </Styled.FormColumn>
+
+      <Styled.FormColumn>
+        <Styled.FormTitle $colors={theme.texto}>Medicamento</Styled.FormTitle>
         {medicine &&
           medicine.map((medicine) => {
             return (
-              <div className="patientRecordsContent" key={medicine.id}>
-                <div className=" PatientRecordsRow">
-                  <h2>Nome do Medicamento: {medicine.name}</h2>
+              <Styled.FormColumn key={medicine.id}>
+                <Styled.FormRow>
+                  <Styled.FormTitle $colors={theme.texto}>
+                    Nome do Medicamento: {medicine.name}
+                  </Styled.FormTitle>
                   <Link
                     className="LinkPatientRecord"
                     to={`/cadastro/exercicio/${medicine.id}`}
                   >
                     Editar
                   </Link>
-                </div>
-                <span>Tipo de medicação:{medicine.medicineType}</span>
-                <span>Quantidade de medicação:{medicine.amount}</span>
-                <span>Unidade de medicação:{medicine.unit}</span>
-                <span>Observações :{medicine.comments}</span>
+                </Styled.FormRow>
+                <Styled.FormSubTitle $colors={theme.texto}>
+                  Tipo de medicação:{medicine.medicineType}
+                </Styled.FormSubTitle>
+                <Styled.FormSubTitle $colors={theme.texto}>
+                  Quantidade de medicação:{medicine.amount}
+                </Styled.FormSubTitle>
+                <Styled.FormSubTitle $colors={theme.texto}>
+                  Unidade de medicação:{medicine.unit}
+                </Styled.FormSubTitle>
+                <Styled.FormSubTitle $colors={theme.texto}>
+                  Observações :{medicine.comments}
+                </Styled.FormSubTitle>
 
-                <div>
-                  <span>Data :{exercise.date}</span>
-                  <span>Horário :{exercise.time}</span>
-                </div>
-              </div>
+                <Styled.ButtonWrapper>
+                  <Styled.FormSubTitle $colors={theme.texto}>
+                    Data :{medicine.date}
+                  </Styled.FormSubTitle>
+                  <Styled.FormSubTitle $colors={theme.texto}>
+                    Horário :{medicine.time}
+                  </Styled.FormSubTitle>
+                </Styled.ButtonWrapper>
+              </Styled.FormColumn>
             );
           })}
-      </div>
-    </section>
+      </Styled.FormColumn>
+    </Styled.Form>
   );
 };
